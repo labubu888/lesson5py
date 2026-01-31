e839f001d957fda3c86baad6f4ae7947abe845d2
labubu888 <tanishqabulani@gmail.com>
2026-01-31 12:06:01 +0400

    Add files via upload

---
+++ fourth.py
@@ -0,0 +1,8 @@
+i = int(input("enter a number : "))
+if (i< 15):
+    print("i is smaller than 15")
+    print("i am in if block")
+else:
+    print("i am greater than 15")  
+    print("i am in else block") 
+print("i am not in if andd not in else block")    
